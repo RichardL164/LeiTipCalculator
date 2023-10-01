dc437bd55ea8190fe74795134cd9513150d8b930
Richard <richardl164@nycstudents.net>
2023-10-01 00:23:14 -0400

    Tried to fix discount system but not successful yet

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -1,19 +1,19 @@
 import java.util.*;
 public class TipCalculator {
     public static void main(String[] args) {
-        ArrayList<String> arr = new ArrayList();
-        Scanner scan = new Scanner(System.in);
+        ArrayList<String> arr = new ArrayList(); //creates list
+        Scanner scan = new Scanner(System.in); //creates scanner
         System.out.println("Welcome to the tip calculator!");
         System.out.print("How many people are in your group? ");
-        int numPeople = scan.nextInt();
+        int numPeople = scan.nextInt(); //gets number of people
         scan.nextLine();
         System.out.print("What is the tip percentage? ");
-        int tipPercent = scan.nextInt();
+        int tipPercent = scan.nextInt(); //gets tip percentage
         scan.nextLine();
 
         double foodCostTotal = 0;
         double prices = 0;
-        while (prices != -1) {
+        while (prices != -1) { //loop goes until user enters -1
             foodCostTotal += prices;
             System.out.print("Enter a cost in dollar and cents (-1 to end): ");
             prices = scan.nextDouble();
@@ -23,29 +23,29 @@ public class TipCalculator {
             }
             System.out.print("Enter the food: ");
             String food = scan.nextLine();
-
             arr.add(food);
-
         }
 
         System.out.println("Do you know our discount code for the day? Hint: All roads lead to ____");
         String code = scan.nextLine();
 
-        double totalTip = (int) (foodCostTotal * (tipPercent / 100.00) * 100) / 100.0;
-        double totalBill = (int) ((totalTip + foodCostTotal) * 100) / 100.0;
+        System.out.println("--------------------");
+        double totalTip = (int) (foodCostTotal * (tipPercent / 100.00) * 100) / 100.0; //gets total tip the customers need to pay
+        System.out.println("Total bill before tip and discount: " + foodCostTotal);
+        System.out.println("Total percentage: " + tipPercent + "%");
+        double totalBill = (int) ((totalTip + foodCostTotal) * 100) / 100.0; //total bill customer needs to pay
         if (code.equals("rome")) {
             System.out.println("Congratulations! You get a 10% discount!");
-            totalBill *= 0.9;
+            totalBill *= (int) ((totalBill * 0.9) * 100)/ 100.0; //10% discount off total bill
+            foodCostTotal *= 0.9;
+            totalTip *= 0.9;
         } else {
             System.out.println("Sorry, that is not the discount code.");
         }
         double beforeTip = (int) (foodCostTotal / numPeople * 100) / 100.0;
-        double tipPerPerson = (int) (totalTip / numPeople * 100) / 100.0;
-        double costPerPerson = (int) (totalBill / numPeople * 100) / 100.0;
+        double tipPerPerson = (int) (totalTip / numPeople * 100) / 100.0; //how much every person needs to tip
+        double costPerPerson = (int) (totalBill / numPeople * 100) / 100.0; //how much everyone needs to pay
 
-        System.out.println("--------------------");
-        System.out.println("Total bill before tip and discount: " + foodCostTotal);
-        System.out.println("Total percentage: " + tipPercent + "%");
         System.out.println("Total tip: " + totalTip);
         System.out.println("Total bill with tip: " + totalBill);
         System.out.println("Per person cost before tip: " + beforeTip);
@@ -54,7 +54,7 @@ public class TipCalculator {
         System.out.println("Items ordered: ");
         System.out.println("--------------------");
         for (int i = 0; i <arr.size(); i++) {
-            System.out.println(arr.get(i));
+            System.out.println(arr.get(i)); // prints out each item in the list
         }
 
     }
